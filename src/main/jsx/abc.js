526e0a7302a52e84f74ce5a159521b96ebe7d94d
Anton Tobolkin <42136387+tobantal@users.noreply.github.com>
2019-12-10 08:33:04 +0700

    Update abc.js

--- src/main/jsx/abc.js
+++ src/main/jsx/abc.js
@@ -1,1 +1,9 @@
-var x = 5
+var a = NaN;
+
+if (a === NaN) {  // Noncompliant; always false
+  console.log("a is not a number");  // this is dead code
+}
+
+if (a !== NaN) { // Noncompliant; always true
+  console.log("a is not NaN"); // this statement is not necessarily true
+}
